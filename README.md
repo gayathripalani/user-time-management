10032ae6c1f597d043ea51568e455493c317b366
Gayathri Palani <gayathri.palani7@gmail.com>
2023-12-05 13:24:39 +0100

    Create the boiler plate

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-My Netflix App
+User Time Management
 
 Development
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-
 function App() {
 
   return (
-    <React.Fragment>
-      <main className='App'>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </main>
-    </React.Fragment>
+    <main className='App'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
   )
 }
 
