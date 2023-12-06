7aaab747dedf852bb30d83805f4a8ba111506d91
Gayathri Palani <gayathri.palani7@gmail.com>
2023-12-06 08:16:52 +0100

    Add and save task for a day

--- src/components/user/AddTimeSheet.tsx
+++ src/components/user/AddTimeSheet.tsx
@@ -1,54 +1,64 @@
 import { useState } from 'react';
 import customerData from '../../utils/customer.json';
 
-const AddTimeSheet = ({register, handleInputChange }) => {
-  const [projects, setProjects] = useState<string[]>([]);
+const AddTimeSheet = ({ register, handleInputChange }) => {
+  const [projects, setProjects] = useState<string[]>(['Reports']);
 
   const handleCustomerChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCustomer = customerData?.customers.find(
       (customer) => customer.name === event.target.value
     );
-    setProjects(selectedCustomer?.projects.map((project) => project.name) || []);
+    const defaultProjects = selectedCustomer?.projects.map((project) => project.name) || [];
+    setProjects(defaultProjects);
   };
 
   return (
+    <>
+      <input
+        className="p-4 my-2 bg-gray-100"
+        placeholder="Description"
+        type="text"
+        {...register('description')}
+        onChange={handleInputChange('description')}
+      />
+      <select
+        className="p-4 my-2 bg-gray-100"
+        {...register('customer', { defaultValue: customerData?.customers[0]?.name })}
+        onChange={handleCustomerChange}
+      >
+        {customerData?.customers.map((customer, index) => (
+          <option key={index} value={customer.name}>
+            {customer.name}
+          </option>
+        ))}
+      </select>
+      <select
+        className="p-4 my-2 bg-gray-100"
+        {...register('project', { defaultValue: projects[0] })}
+      >
+        {projects.map((project, index) => (
+          <option key={index} value={project}>
+            {project}
+          </option>
+        ))}
+      </select>
 
-   <> <input
-    className="p-4 my-2 bg-gray-100"
-    placeholder="Description"
-    type="text"
-    {...register('description')}
-    onChange={handleInputChange('description')}
-  />
-  <select className="p-4 my-2 bg-gray-100" {...register('customer')} onChange={handleCustomerChange}>
-    {customerData?.customers.map((customer, index) => (
-      <option key={index} value={customer.name}>
-        {customer.name}
-      </option>
-    ))}
-  </select>
-  <select className="p-4 my-2 bg-gray-100" {...register('project')}>
-    {projects.map((project, index) => (
-      <option key={index} value={project}>
-        {project}
-      </option>
-    ))}
-  </select>
-  <input
-    className="p-4 my-2 bg-gray-100"
-    type="text"
-    placeholder="Hours"
-    {...register('hours')}
-    onChange={handleInputChange('hours')}
-  />
-  <input
-    className="p-4 my-2 bg-gray-100"
-    type="text"
-    placeholder="Comment"
-    {...register('comment')}
-    onChange={handleInputChange('comment')}
-  /></>
-  )
-}
+      <input
+        className="p-4 my-2 bg-gray-100"
+        type="text"
+        placeholder="Hours"
+        {...register('hours')}
+        onChange={handleInputChange('hours')}
+      />
+      <input
+        className="p-4 my-2 bg-gray-100"
+        type="text"
+        placeholder="Comment"
+        {...register('comment')}
+        onChange={handleInputChange('comment')}
+      />
+    </>
+  );
+};
 
-export default AddTimeSheet
+export default AddTimeSheet;
--- src/components/user/TimeEntryForm.tsx
+++ src/components/user/TimeEntryForm.tsx
@@ -1,26 +1,24 @@
-// TimeEntryForm.tsx
 import { FC, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, UseFormRegister } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { addEntry, setTaskCount } from '../../utils/timeSheetEntrySlice';
+import { addEntry, setTaskCount } from '../../utils/timesheetEntrySlice';
 import AddTimeSheet from './AddTimeSheet';
 import { TimeSheetEntry, TimeEntry } from '../../utils/type';
-
-type FormValues = {
-  timeEntries: TimeEntry[];
-};
 
 const TimeEntryForm: FC = () => {
   const dispatch = useDispatch();
   const [taskCount, setTaskCountLocal] = useState(0);
+
   const {
     formState: { errors, isValid },
     register,
     handleSubmit,
     setValue,
     getValues,
-  } = useForm<FormValues>({
+  } = useForm<TimeSheetEntry>({
     defaultValues: {
+      uid: 1,
+      date: '2023-12-07',
       timeEntries: [
         {
           description: 'Tracking',
@@ -34,7 +32,7 @@ const TimeEntryForm: FC = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+  const onSubmit: SubmitHandler<TimeSheetEntry> = async (data) => {
     try {
       dispatch(addEntry(data.timeEntries));
     } catch (error) {
@@ -54,14 +52,14 @@ const TimeEntryForm: FC = () => {
 
   const handleAddTaskClick = () => {
     const currentEntries = getValues().timeEntries || [];
-    dispatch(setTaskCount(taskCount + 1)); // Corrected
-    setTaskCountLocal(taskCount + 1);
+    dispatch(setTaskCount(taskCount + 1)); // Dispatch the action to update the Redux store
+    setTaskCountLocal(taskCount + 1); // Local state for UI updates
     setValue('timeEntries', [...currentEntries, {}]);
   };
 
   return (
     <form className="mt-20 px-10" onSubmit={handleSubmit(onSubmit)}>
-      {getValues().timeEntries.map((entry, index) => (
+      {getValues()?.timeEntries.map((entry, index) => (
         <div key={index} className="flex flex-row justify-between mb-4">
           <input
             className="p-4 bg-gray-100"
@@ -86,13 +84,15 @@ const TimeEntryForm: FC = () => {
         Add task
       </button>
 
-      <button
-        className="p-2 my-4 bg-black rounded-lg text-white"
-        type="submit"
-        disabled={!isValid}
-      >
-        Save
-      </button>
+      <div className="flex justify-center">
+        <button
+          className="py-2 px-4 my-4 bg-black rounded-lg text-white"
+          type="submit"
+          disabled={!isValid}
+        >
+          Save
+        </button>
+      </div>
     </form>
   );
 };
